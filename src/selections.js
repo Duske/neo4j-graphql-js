1b041eb4912b21f534aae7374e2a0f550249d775
Dustin <dustin.duske@gmail.com>
2018-09-11 11:50:12 +0200

    (prototype) inlinefragment support

--- src/selections.js
+++ src/selections.js
@@ -101,10 +101,99 @@ export function buildCypherSelection({
     });
   }
 
+
   // We have a graphql object type
 
   const nestedVariable = variableName + '_' + fieldName;
   const skipLimit = computeSkipLimit(headSelection, resolveInfo.variableValues);
+
+  if(innerSchemaType && innerSchemaType.astNode && innerSchemaType.astNode.kind === "InterfaceTypeDefinition") {
+      const interfaceName = innerSchemaType.name;
+      // get common selections
+      const commonSelections = headSelection.selectionSet.selections.filter(item => item.kind === "Field");
+      // get the inlinefragments
+      const fragments = headSelection.selectionSet.selections.filter(item => item.kind === "InlineFragment");
+
+      // create an object for each fragment containing type, selections and subselections
+      const fragmentMap = fragments.map(fragment => {
+        // name of the current fragment
+        const implementationName = fragment.typeCondition.name.value;
+        // merge selections
+        const selections = [...commonSelections, ...fragment.selectionSet.selections];
+        // get the graphqlObjectType for our fragment
+        const schemaType = resolveInfo.schema._implementations[interfaceName].find(intfc => intfc.name === implementationName);
+
+        // generate subselections
+        const subSelections = extractSelections(selections, resolveInfo.fragments);
+        const subSelection = recurse({
+            initial: '',
+            selections: subSelections,
+            variableName: nestedVariable + '_frag' + implementationName,
+            schemaType: schemaType,
+            resolveInfo
+        });
+        return {
+          selections: selections,
+          name: implementationName,
+          // get graphql schema type for implementation
+          type: schemaType,
+          subSelection: subSelection
+        };
+      });
+
+      let selection;
+
+      if (customCypher) {
+          // similar: [ x IN apoc.cypher.runFirstColumn("WITH {this} AS this MATCH (this)--(:Genre)--(o:Movie) RETURN o", {this: movie}, true) |x {.title}][1..2])
+          const fieldIsList = !!fieldType.ofType;
+
+          selection = recurse({
+              initial: `${initial}${fieldName}: ${
+                  fieldIsList ? '' : 'head('
+                  }[ ${nestedVariable} IN apoc.cypher.runFirstColumn("${customCypher}", ${cypherDirectiveArgs(
+                  variableName,
+                  headSelection,
+                  schemaType,
+                  resolveInfo
+              )}, true) | ${nestedVariable} {${subSelection[0]}}]${
+                  fieldIsList ? '' : ')'
+                  }${skipLimit} ${commaIfTail}`,
+              ...tailParams
+          });
+      } else {
+          // graphql object type, no custom cypher
+
+          const { name: relType, direction: relDirection } = relationDirective(
+              schemaType,
+              fieldName
+          );
+
+          const queryParams = innerFilterParams(filterParams);
+
+          selection = recurse({
+              initial: `${initial}${fieldName}: ${
+                  !isArrayType(fieldType) ? 'head(' : ''
+                  }${
+                    fragmentMap.map((fragment) => {
+                      const varName = nestedVariable + '_frag' + fragment.name;
+                      return `[(${variableName})${
+                          relDirection === 'in' || relDirection === 'IN' ? '<' : ''
+                          }-[:${relType}]-${
+                          relDirection === 'out' || relDirection === 'OUT' ? '>' : ''
+                          }(${varName}:${
+                          fragment.name
+                          }${queryParams}) | ${varName} {${fragment.subSelection[0]}}]`
+                    }).join(' + ')
+                  }${
+                  !isArrayType(fieldType) ? ')' : ''
+                  }${skipLimit} ${commaIfTail}`,
+              ...tailParams
+          });
+      }
+
+      return [selection[0], { ...selection[1], ...fragmentMap.map(fragment => fragment.subSelection[1]) }];
+  }
+
 
   const subSelections = extractSelections(
     headSelection.selectionSet.selections,
